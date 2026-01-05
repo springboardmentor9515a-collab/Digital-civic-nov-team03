6c030d0f6f0f13300cd719a36e6be2a4fb21a365
vaishnavivsb <vaishnavivsb1701@gmail.com>
2026-01-05 17:24:03 +0530

    Clean backend setup for poll APIs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const petitionRoutes = require("./routes/petitionRoutes");
+
 const pollRoutes = require("./routes/pollRoutes");
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/petitions", petitionRoutes);
+
 app.use("/api/polls", pollRoutes);
 
 mongoose
